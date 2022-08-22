6d003761001e08cc998e1e8b85ba7402ccac0eb6
artem.chernomazov <pechenkazz@mail.ru>
2022-08-23 00:22:49 +0300

    Exercise04

---
+++ Exercise04/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Введите число");
+int x = Convert.ToInt32(Console.ReadLine());
+int y = 2;
+if(x==y)
+{
+    Console.WriteLine(y);
+}
+while(y<x)
+{
+    Console.Write(y + ", ");
+    y=y+2;
+    
+}
